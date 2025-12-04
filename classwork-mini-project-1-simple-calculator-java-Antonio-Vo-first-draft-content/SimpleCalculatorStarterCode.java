9482501fd5926728b394d7e8305f094e55bb0d27
Antonio-Vo <34730@ocvts.org>
2025-12-04 09:19:25 -0500

    Add files via upload

---
+++ classwork-mini-project-1-simple-calculator-java-Antonio-Vo-first-draft-content/SimpleCalculatorStarterCode.java
@@ -0,0 +1,65 @@
+/* 
+NAME: Trisha Smith 
+DATE: 9/14/2025 
+
+PROJECT BRIEF: 
+
+Goal:
+Learn methods, switch statements, and error handling.
+Skills Learned:
+Writing methods for modular code
+Using if,else if,else or switch to handle different operators
+Basic error handling with try/catch
+Looping to allow repeated calculations
+
+
+Concept:
+● User enters two numbers and an operator (+, -, ×, ÷).
+● Program performs the operation and outputs the result.
+● Program repeats until the user chooses to quit.
+
+
+Checklist:
+ ✔ Can write separate methods for each operation (+, -, ×, ÷)
+ ✔ Can use if,else if,else or switch  to handle operators
+ ✔ Can validate division by zero
+ ✔ Can loop until the user quits
+ ✔ Can handle invalid input with try/catch
+
+Challenge:
+● Add power and exponents (x², x^y)
+● Add square root operations (√)
+● Bonus Challenge (choose one):
+● Add Advanced Operations:
+● Add support for: Factorial and Percentages
+● Trigonometric Functions (sin, cos, tan).
+
+*/ 
+import java.util.*; 
+public class SimpleCalculatorStarterCode{ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);  
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+        System.out.print("Hello " + name + " :D");
+
+        /* 
+            ✔️// User Input - User Input need a scanner 
+            - Ask user the user for two numbers 
+
+            // Operators - List??
+            Need Addition 
+            Need Suntration 
+            Need Multiplication
+            Need Division 
+
+            // Add dialog 
+            // confirm the equation that was seletction
+            // Make sure the math is mathing (making sure it complies correctly) 
+
+            // Print the results
+        */ 
+        
+
+    }
+} 
